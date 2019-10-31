9321371d3c2395071c905c5f8a6e2c7b1a0bf56e
Didzis Tupureins <d.tupureins@inbox.lv>
2019-10-31 12:55:03 +0200

    Initial Commit

---
+++ TemperatureConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemperatureConverter
+//
+//  Created by Didzis Tupureins on 31/10/2019.
+//  Copyright © 2019 Didzis Tupureins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
